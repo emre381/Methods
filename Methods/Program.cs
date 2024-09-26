0c136cb51e572a1c51d04467d453c12709d75b2b
emre381 <cihatemre381@gmail.com>
2024-09-26 16:28:35 +0300

    first commit

---
+++ Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string isimSoyad;
+            int yas;
+            double boy;
+
+            Console.WriteLine("Lütfen değerleri girin isim soy isim");
+            isimSoyad = Console.ReadLine();
+            Console.WriteLine("Lütfen yaşınızı gitiniz:");
+            yas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Lütfen boyunuzu metre cinsinden giriniz:");
+            boy = double.Parse(Console.ReadLine());
+            isimSoyle(isimSoyad , yas , boy);
+
+            dogunYiliHesapla(yas);
+            Console.ReadLine();
+        }
+
+       
+        static void isimSoyle(string i, int y, double b)
+        {
+            Console.WriteLine("Adı ve Soyadı : {0} Yaşınız :{1} Boyunuz {2} ",i,y,b);
+        }
+
+        static void dogunYiliHesapla(int yas)
+        {
+            Console.WriteLine("Doğum tarihiniz:" + (2024 - yas));  
+        }
+    }
+}
